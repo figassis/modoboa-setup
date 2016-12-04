fa3a87a5191e10ec86397987f9f06ba2f470f331
Assis Ngolo <figassis@nellcorp.com>
2016-12-04 11:09:16 +0100

    made setup executable

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install python-pip python-rrdtool python-mysqldb python-dev libcairo2-dev ibpango1.0-dev librrd-dev libxml2-dev libxslt-dev zlib1g-dev
+sudo apt-get install -y python-pip python-rrdtool python-mysqldb python-dev libcairo2-dev ibpango1.0-dev librrd-dev libxml2-dev libxslt-dev zlib1g-dev
 git clone https://github.com/modoboa/modoboa-installer
 mv installer.cfg modoboa-installer/installer.cfg
 cd modoboa-installer
