a547f7fe9370a0adc5fd8c7a98491f8dc64a8b74
Assis Ngolo <figassis@nellcorp.com>
2016-12-05 11:27:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ domain=$1
 
 sudo apt-get install -y python-pip python-rrdtool python-mysqldb python-dev libcairo2-dev ibpango1.0-dev librrd-dev libxml2-dev libxslt-dev zlib1g-dev
 git clone https://github.com/modoboa/modoboa-installer
-mv installer.cfg modoboa-installer/installer.cfg
+cp installer.cfg modoboa-installer/installer.cfg
 cd modoboa-installer
 
 sudo sed -i 's|mysql_password|'`openssl rand -base64 32`'|g' installer.cfg
