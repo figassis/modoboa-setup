81aebe4305763b2fbcbba09500db8eb84610d9a0
Assis Ngolo <figassis@nellcorp.com>
2020-07-04 21:34:44 +0000

    updates

--- firewall.sh
+++ firewall.sh
@@ -8,14 +8,6 @@ sudo ufw allow 25
 sudo ufw allow 443
 sudo ufw allow 993
 sudo ufw allow 80
+sudo ufw allow 3306
 
-sudo ufw allow from 127.0.0.1 to any port 143
-sudo ufw allow from 127.0.0.1 to any port 10024
-sudo ufw allow from 127.0.0.1 to any port 10025
-sudo ufw allow from 127.0.0.1 to any port 10026
-sudo ufw allow from 127.0.0.1 to any port 9998
-sudo ufw allow from 127.0.0.1 to any port 34607
-sudo ufw allow from 127.0.0.1 to any port 58278
-sudo ufw allow from 127.0.0.1 to any port 3306
-sudo ufw allow from 107.170.24.173 to any port 3306
 sudo ufw enable
